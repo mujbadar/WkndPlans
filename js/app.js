de73d031dc666defce415a2c250ee129e738d0da
Mujtaba Badar <mujbadar@macbook-pro.lan>
2019-04-29 23:12:04 -0500

    finished weather and event carousel

--- js/app.js
+++ js/app.js
@@ -39,39 +39,40 @@ const getWeather = () => {
     console.log(weatherData);
     $('.weather-card1').html(`
       <h1> ${weatherData.city.name}  </h1>
-      <h3> ${weatherData.list[0].dt_txt}  </h3>
-      <h5> ${weatherData.list[0].main.temp}F  </h5>
-      <h5> ${weatherData.list[0].weather[0].description}  </h5>
+      <h3> Date/Time: ${weatherData.list[0].dt_txt}  </h3>
+      <h5> Temp: ${weatherData.list[0].main.temp}F  </h5>
+      <h5> Weather Conditions: ${weatherData.list[0].weather[0].description}  </h5>
       <h5> ${weatherData.list[0].weather[0].icon}  </h5>
       `).addClass('weatherClass')
     $('.weather-card2').html(`
       <h1> ${weatherData.city.name}  </h1>
-      <h3> ${weatherData.list[8].dt_txt}  </h3>
-      <h5> ${weatherData.list[8].main.temp}F  </h5>
-      <h5> ${weatherData.list[8].weather[0].description}  </h5>
+      <h3> Date/Time: ${weatherData.list[8].dt_txt}  </h3>
+      <h5> Temp: ${weatherData.list[8].main.temp}F  </h5>
+      <h5> Weather Conditions: ${weatherData.list[8].weather[0].description}  </h5>
       <h5> ${weatherData.list[8].weather[0].icon}  </h5>
       `).addClass('weatherClass')
     $('.weather-card3').html(`
       <h1> ${weatherData.city.name}  </h1>
-      <h3> ${weatherData.list[16].dt_txt}  </h3>
-      <h5> ${weatherData.list[16].main.temp}F  </h5>
-      <h5> ${weatherData.list[16].weather[0].description}  </h5>
+      <h3> Date/Time: ${weatherData.list[16].dt_txt}  </h3>
+      <h5> Temp: ${weatherData.list[16].main.temp}F  </h5>
+      <h5> Weather Conditions: ${weatherData.list[16].weather[0].description}  </h5>
       <h5> ${weatherData.list[16].weather[0].icon}  </h5>
       `).addClass('weatherClass')
     $('.weather-card4').html(`
       <h1> ${weatherData.city.name}  </h1>
-      <h3> ${weatherData.list[24].dt_txt}  </h3>
-      <h5> ${weatherData.list[24].main.temp}F  </h5>
-      <h5> ${weatherData.list[24].weather[0].description}  </h5>
+      <h3> Date/Time: ${weatherData.list[24].dt_txt}  </h3>
+      <h5> Temp: ${weatherData.list[24].main.temp}F  </h5>
+      <h5> Weather Conditions: ${weatherData.list[24].weather[0].description}  </h5>
       <h5> ${weatherData.list[24].weather[0].icon}  </h5>
       `).addClass('weatherClass')
     $('.weather-card5').html(`
       <h1> ${weatherData.city.name}  </h1>
-      <h3> ${weatherData.list[32].dt_txt}  </h3>
-      <h5> ${weatherData.list[32].main.temp}F  </h5>
-      <h5> ${weatherData.list[32].weather[0].description}  </h5>
+      <h3> Date/Time: ${weatherData.list[32].dt_txt}  </h3>
+      <h5> Temp: ${weatherData.list[32].main.temp}F  </h5>
+      <h5> Weather Conditions: ${weatherData.list[32].weather[0].description}  </h5>
       <h5> ${weatherData.list[32].weather[0].icon}  </h5>
       `).addClass('weatherClass')
+  
   })
 }
 
@@ -89,74 +90,74 @@ const getEvent = () => {
     success: function(json) {
               console.log(json);
               $('.event-1').html(`
-                <h1> ${json._embedded.events[0].name}  </h1>
-                <h4> ${json._embedded.events[0].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[0].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[0].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[0]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[0].name}</h1>
+                <h4> Genre: ${json._embedded.events[0].classifications[0].segment.name}</h4>
+                <h4> Event type: ${json._embedded.events[0].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[0].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[0]._embedded.venues[0].name}<h4>
               `).addClass('eventClass'),
               $('.event-2').html(`
-                <h1> ${json._embedded.events[1].name}  </h1>
-                <h4> ${json._embedded.events[1].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[1].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[1].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[1]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[1].name}</h1>
+                <h4> Genre: ${json._embedded.events[1].classifications[0].segment.name}</h4>
+                <h4> Event type:  ${json._embedded.events[1].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[1].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[1]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
               $('.event-3').html(`
-                <h1> ${json._embedded.events[2].name}  </h1>
-                <h4> ${json._embedded.events[2].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[2].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[2].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[2]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[2].name}</h1>
+                <h4> Genre:  ${json._embedded.events[2].classifications[0].segment.name}</h4>
+                <h4> Event type:  ${json._embedded.events[2].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[2].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[2]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
               $('.event-4').html(`
-                <h1> ${json._embedded.events[3].name}  </h1>
-                <h4> ${json._embedded.events[3].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[3].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[3].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[3]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[3].name}</h1>
+                <h4> Genre: ${json._embedded.events[3].classifications[0].segment.name}</h4>
+                <h4> Event type:  ${json._embedded.events[3].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[3].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[3]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
               $('.event-5').html(`
-                <h1> ${json._embedded.events[4].name}  </h1>
-                <h4> ${json._embedded.events[4].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[4].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[4].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[4]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[4].name}</h1>
+                <h4> Genre: ${json._embedded.events[4].classifications[0].segment.name}</h4>
+                <h4> Event type:  ${json._embedded.events[4].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[4].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[4]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
               $('.event-6').html(`
-                <h1> ${json._embedded.events[5].name}  </h1>
-                <h4> ${json._embedded.events[5].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[5].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[5].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[5]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[5].name}</h1>
+                <h4> Genre:  ${json._embedded.events[5].classifications[0].segment.name}</h4>
+                <h4> Event type:  ${json._embedded.events[5].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[5].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[5]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
               $('.event-7').html(`
-                <h1> ${json._embedded.events[6].name}  </h1>
-                <h4> ${json._embedded.events[6].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[6].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[6].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[6]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[6].name}</h1>
+                <h4> Genre: ${json._embedded.events[6].classifications[0].segment.name}</h4>
+                <h4> Event type: ${json._embedded.events[6].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[6].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[6]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
               $('.event-8').html(`
-                <h1> ${json._embedded.events[7].name}  </h1>
-                <h4> ${json._embedded.events[7].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[7].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[7].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[7]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[7].name}</h1>
+                <h4> Genre: ${json._embedded.events[7].classifications[0].segment.name}</h4>
+                <h4> Event type:  ${json._embedded.events[7].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[7].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[7]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
               $('.event-9').html(`
-                <h1> ${json._embedded.events[8].name}  </h1>
-                <h4> ${json._embedded.events[8].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[8].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[8].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[8]._embedded.venues[0].name} <h4>
+                <h1> ${json._embedded.events[8].name}</h1>
+                <h4> Genre: ${json._embedded.events[8].classifications[0].segment.name}</h4>
+                <h4> Event type: ${json._embedded.events[8].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[8].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[8]._embedded.venues[0].name}<h4>
                 `).addClass('eventClass')
               $('.event-10').html(`
                 <h1> ${json._embedded.events[9].name}  </h1>
-                <h4> ${json._embedded.events[9].classifications[0].segment.name}  </h4>
-                <h4> ${json._embedded.events[9].classifications[0].genre.name}  </h4>
-                <h4> ${json._embedded.events[9].dates.start.localDate} <h4>
-                <h4> ${json._embedded.events[9]._embedded.venues[0].name} <h4>
+                <h4> Genre: ${json._embedded.events[9].classifications[0].segment.name}</h4>
+                <h4> Event type:  ${json._embedded.events[9].classifications[0].genre.name}</h4>
+                <h4> Date: ${json._embedded.events[9].dates.start.localDate}<h4>
+                <h4> Venue: ${json._embedded.events[9]._embedded.venues[0].name}<h4>
               `).addClass('eventClass')
          },
           error: function(xhr, status, err) {
@@ -170,6 +171,11 @@ $(() => {
 // Weather Carousel Variables
 let currentWeatherIndex = 0
 const highestWeatherIndex = $('.weather-info').children().length - 1;
+
+// Event Carousel Variables
+let currentEventIndex = 0
+const highestEventIndex = $('.event-info').children().length - 1;
+
 //////////////
 // Event Listener
 /////////////////
@@ -209,6 +215,31 @@ $('#prev-weather').on('click', () => {
     $('.weather-info').children().eq(currentWeatherIndex).css('display', 'block');
   })
 
-  
+// Event Carousel
+///////////
+
+// Next button
+$('#next-event').on('click', () => {
+  console.log('next event tile');
+  $('.event-info').children().eq(currentEventIndex).css('display', 'none')
+    if (currentEventIndex < highestEventIndex) {
+      currentEventIndex++;
+    } else {
+      currentEventIndex = 0;
+    }
+  $('.event-info').children().eq(currentEventIndex).css('display', 'block')
+})
+
+// Previous button
+$('#prev-event').on('click', () => {
+  console.log('previous weather tile');
+  $('.event-info').children().eq(currentEventIndex).css('display', 'none');
+    if (currentEventIndex > 0) {
+      currentEventIndex--;
+    } else {
+      currentEventIndex = highestEventIndex;
+    }
+    $('.event-info').children().eq(currentEventIndex).css('display', 'block');
+  })
 
 })
