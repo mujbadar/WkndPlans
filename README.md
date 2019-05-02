75f256f476cbc6f093d32a980afda0011c28b51b
Mujtaba Badar <mujbadar@macbook-pro.lan>
2019-05-02 14:22:19 -0500

    Swiper plugin for the Carousel completed. Code has been rearranged to properly account for the scope of the document loading.

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
 # WkndPlans
 This is my first project for SEI Remote Flex
 
+![Image of wireframe](/images/WkndPlans-wireframe.png)
+
+
 // Summary
 ////////////////
-WkndPlans is a single page webapp that will display 5-day forecast and events in the users local area. The user can input their zipcode into the input box and gain access to vital information to plan out their weekend. This information includes a carousel with a 5-day forecast as well as another carousel that lists local events through TicketMaster. 
+WkndPlans is a single page webapp that will display 5-day forecast and events in the user's local area. The user can input their zipcode into the input box and gain access to vital information to plan out their weekend. This information includes a carousel with a 5-day forecast as well as another carousel that lists local events through TicketMaster.
 
 //Technology Used
 //////////////////
-I will be using to HTML, CSS, and Javascript to make this app. I will be using jQuery as a library and will two API, one for weather and one from ticketmaster
+This app is coded in HTML, CSS, and Javascript using jQuery as a library. Two APIs were used to the weather and events (openweathermap and ticketmaster). An external css plugin was used for the carousel.  
 
-![alt text](https://github.com/mujbadar/WkndPlans/issues/1)
+// Weather API
+The API used is "Open Weather Map" API. This API can be accessed with the URL:
+https://api.openweathermap.org/
+
+This API was able to pull the current temperature, weather conditions, and icons. The information will be appended to a carousel and give a 5-day forecast in the user's inputted area. The user can change the zip code and access new weather information.
+
+// TicketMaster API
+The API used is "TicketMaster Discovery". This API can be accessed with the URL:
+https://app.ticketmaster.com/discovery/
+
+This API was able to pull current event with tickets available through TicketMaster. These events are to be appended to the carousel and broken up by their genre, type of event, date, venue, and event banners provided by a URL link from TicketMaster. 10 events are to be displayed to the user.
+
+// Swiper Carousel (plugin)
+Swiper carousel was used as a 3D animated carousel displaying weather and event information. It is responsive to both mobile and desktop devices. This plugin can be accessed with the URL:
+http://idangero.us/swiper/demos/
+
+The 3d cover-flow effect was used and its source code can be found at:
+https://github.com/nolimits4web/Swiper/blob/master/demos/240-effect-coverflow.html
+
+The carousel is to be integrated with the app and should contain the relevant information pulled from both the TicketMaster API and the OpenWeatherMap API.
--- js/app.js
+++ js/app.js
@@ -1,31 +1,11 @@
-console.log('Hello');
-console.log($);
-////////////
-// App Preloader
-///////////////
-
-// Hide the body elements of the app until user inputs zip
-$('.weather-results').hide();
-$('.event-results').hide();
-
 /////////////
 //API Vaiables
 ////////////
-let cityCode = ''
-// Zip code input for both API calls
-$('#submit').on('click', (event) => {
-  event.preventDefault()
-  cityCode = $('#text-box').val()
-  console.log(cityCode);
-  $('.weather-results').show();
-  $('.event-results').show();
-  getWeather();
-  getEvent();
-  window.scrollBy(0,600).slow()
-});
 
-console.log(cityCode);
-// Variables for weather API
+// User input Zip. This is used for both api's
+let cityCode = ''
+
+// Variables for weather api
 const baseWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast?zip='
 const apiWeatherKey = '&appid=ea88b0227f0e326c371545da29d5c540'
 
@@ -35,57 +15,103 @@ const baseEventURL = 'https://app.ticketmaster.com/discovery/v2/events.json?'
 const apiEventKey = 'apikey=VpjGo4Q2pHG4BhxfhbmP4bCct9xSOGjf'
 let eventURL = baseEventURL + apiEventKey + '&postalCode='
 
+/////////////
+// Event handlers
+///////////////////
+
+// Zip code input for both api calls
+$('#submit').on('click', (event) => {
+  event.preventDefault()
+  cityCode = $('#text-box').val()
+  console.log(cityCode);
+  $('.weather-results').show();
+  $('.event-results').show();
+  getWeather();
+  getEvent();
+});
+
+console.log(cityCode);
+
+//////////
+// External Plugins
+/////////////
+
+// Carousel external plugin. Swiper plugin
+const swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+////////////
+// App Preloader
+///////////////
+
+// Hide the body elements of the app until user inputs zip
+$('.weather-results').hide();
+$('.event-results').hide();
 
 ///////////////////
-// API calls functions
-///////////////////////////////
+// Functions for AJAX
+////////////////////////
 
 // Weather API
 //////////////
 
 // 5-Day forecast. Use the inputed Zip code to make an API call returning city
 // name, date, temp, current weather conditions, and weather icon.
-// The daily 9am forecast is accessed and appended to 'cards' for DOM.
+// The daily forecast is accessed and appended to 'cards' for DOM.
 const getWeather = () => {
   $.ajax({
     url : baseWeatherURL + cityCode + apiWeatherKey + '&units=imperial'
   }).then((weatherData) => {
     console.log(weatherData);
-    $('.weather-card1').html(`
-      <h1> ${weatherData.city.name}  </h1>
-      <h3> Date/Time: ${weatherData.list[0].dt_txt}  </h3>
-      <h5> Temp: ${weatherData.list[0].main.temp}°F  </h5>
-      <h5> Weather Conditions: ${weatherData.list[0].weather[0].description}  </h5>
+    $('#weather-card1').html(`
+      <h2> ${weatherData.city.name}</h2>
+      <h3> Date/Time: ${weatherData.list[0].dt_txt}</h3>
+      <h5> Temp: ${weatherData.list[0].main.temp}°F</h5>
+      <h5> Weather Conditions: ${weatherData.list[0].weather[0].description}</h5>
       <img src='http://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png' alt="Weather Icon">
-      `).addClass('weatherClass')
-    $('.weather-card2').html(`
-      <h1> ${weatherData.city.name}  </h1>
-      <h3> Date/Time: ${weatherData.list[8].dt_txt}  </h3>
-      <h5> Temp: ${weatherData.list[8].main.temp}°F  </h5>
-      <h5> Weather Conditions: ${weatherData.list[8].weather[0].description}  </h5>
+      `)
+    $('#weather-card2').html(`
+      <h2> ${weatherData.city.name}</h2>
+      <h3> Date/Time: ${weatherData.list[8].dt_txt}</h3>
+      <h5> Temp: ${weatherData.list[8].main.temp}°F</h5>
+      <h5> Weather Conditions: ${weatherData.list[8].weather[0].description}</h5>
       <img src='http://openweathermap.org/img/w/${weatherData.list[8].weather[0].icon}.png' alt="Weather Icon">
-      `).addClass('weatherClass')
-    $('.weather-card3').html(`
-      <h1> ${weatherData.city.name}  </h1>
+      `)
+    $('#weather-card3').html(`
+      <h2> ${weatherData.city.name}</h2>
       <h3> Date/Time: ${weatherData.list[16].dt_txt}  </h3>
       <h5> Temp: ${weatherData.list[16].main.temp}°F  </h5>
-      <h5> Weather Conditions: ${weatherData.list[16].weather[0].description}  </h5>
+      <h5> Weather Conditions: ${weatherData.list[16].weather[0].description}</h5>
       <img src='http://openweathermap.org/img/w/${weatherData.list[16].weather[0].icon}.png' alt="Weather Icon">
-      `).addClass('weatherClass')
-    $('.weather-card4').html(`
-      <h1> ${weatherData.city.name}  </h1>
-      <h3> Date/Time: ${weatherData.list[24].dt_txt}  </h3>
-      <h5> Temp: ${weatherData.list[24].main.temp}°F  </h5>
-      <h5> Weather Conditions: ${weatherData.list[24].weather[0].description}  </h5>
+      `)
+    $('#weather-card4').html(`
+      <h2> ${weatherData.city.name}</h2>
+      <h3> Date/Time: ${weatherData.list[24].dt_txt}</h3>
+      <h5> Temp: ${weatherData.list[24].main.temp}°F</h5>
+      <h5> Weather Conditions: ${weatherData.list[24].weather[0].description}</h5>
       <img src='http://openweathermap.org/img/w/${weatherData.list[24].weather[0].icon}.png' alt="Weather Icon">
-      `).addClass('weatherClass')
-    $('.weather-card5').html(`
-      <h1> ${weatherData.city.name}  </h1>
-      <h3> Date/Time: ${weatherData.list[32].dt_txt}  </h3>
-      <h5> Temp: ${weatherData.list[32].main.temp}°F  </h5>
-      <h5> Weather Conditions: ${weatherData.list[32].weather[0].description}  </h5>
+      `)
+    $('#weather-card5').html(`
+      <h2> ${weatherData.city.name}</h2>
+      <h3> Date/Time: ${weatherData.list[32].dt_txt}</h3>
+      <h5> Temp: ${weatherData.list[32].main.temp}°F</h5>
+      <h5> Weather Conditions: ${weatherData.list[32].weather[0].description}</h5>
       <img src='http://openweathermap.org/img/w/${weatherData.list[32].weather[0].icon}.png' alt="Weather Icon">
-      `).addClass('weatherClass')
+      `)
 
   })
 
@@ -95,7 +121,8 @@ const getWeather = () => {
 ////////////////
 
 // Get 10 of the most recent local events. Display event name, genre, event type,
-// date, and location.
+// date, location, and event image.
+// The events are retrieved and appended to DOM.
 const getEvent = () => {
   $.ajax({
     type:"GET",
@@ -104,86 +131,86 @@ const getEvent = () => {
     dataType: "json",
     success: function(json) {
               console.log(json);
-              $('.event-1').html(`
-                <h1> ${json._embedded.events[0].name}</h1>
+              $('#event-1').html(`
+                <h2> ${json._embedded.events[0].name}</h2>
                 <h4> Genre: ${json._embedded.events[0].classifications[0].segment.name}</h4>
                 <h4> Event type: ${json._embedded.events[0].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[0].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[0]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[0].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass'),
-              $('.event-2').html(`
-                <h1> ${json._embedded.events[1].name}</h1>
+              `)
+              $('#event-2').html(`
+                <h2> ${json._embedded.events[1].name}</h2>
                 <h4> Genre: ${json._embedded.events[1].classifications[0].segment.name}</h4>
                 <h4> Event type:  ${json._embedded.events[1].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[1].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[1]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[1].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
-              $('.event-3').html(`
-                <h1> ${json._embedded.events[2].name}</h1>
+              `)
+              $('#event-3').html(`
+                <h2> ${json._embedded.events[2].name}</h2>
                 <h4> Genre:  ${json._embedded.events[2].classifications[0].segment.name}</h4>
                 <h4> Event type:  ${json._embedded.events[2].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[2].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[2]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[2].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
-              $('.event-4').html(`
-                <h1> ${json._embedded.events[3].name}</h1>
+              `)
+              $('#event-4').html(`
+                <h2> ${json._embedded.events[3].name}</h2>
                 <h4> Genre: ${json._embedded.events[3].classifications[0].segment.name}</h4>
                 <h4> Event type:  ${json._embedded.events[3].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[3].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[3]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[3].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
-              $('.event-5').html(`
-                <h1> ${json._embedded.events[4].name}</h1>
+              `)
+              $('#event-5').html(`
+                <h2> ${json._embedded.events[4].name}</h2>
                 <h4> Genre: ${json._embedded.events[4].classifications[0].segment.name}</h4>
                 <h4> Event type:  ${json._embedded.events[4].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[4].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[4]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[4].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
-              $('.event-6').html(`
-                <h1> ${json._embedded.events[5].name}</h1>
+              `)
+              $('#event-6').html(`
+                <h2> ${json._embedded.events[5].name}</h2>
                 <h4> Genre:  ${json._embedded.events[5].classifications[0].segment.name}</h4>
                 <h4> Event type:  ${json._embedded.events[5].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[5].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[5]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[5].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
-              $('.event-7').html(`
-                <h1> ${json._embedded.events[6].name}</h1>
+              `)
+              $('#event-7').html(`
+                <h2> ${json._embedded.events[6].name}</h2>
                 <h4> Genre: ${json._embedded.events[6].classifications[0].segment.name}</h4>
                 <h4> Event type: ${json._embedded.events[6].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[6].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[6]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[6].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
-              $('.event-8').html(`
-                <h1> ${json._embedded.events[7].name}</h1>
+              `)
+              $('#event-8').html(`
+                <h2> ${json._embedded.events[7].name}</h2>
                 <h4> Genre: ${json._embedded.events[7].classifications[0].segment.name}</h4>
                 <h4> Event type:  ${json._embedded.events[7].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[7].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[7]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[7].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
-              $('.event-9').html(`
-                <h1> ${json._embedded.events[8].name}</h1>
+              `)
+              $('#event-9').html(`
+                <h2> ${json._embedded.events[8].name}</h2>
                 <h4> Genre: ${json._embedded.events[8].classifications[0].segment.name}</h4>
                 <h4> Event type: ${json._embedded.events[8].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[8].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[8]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[8].images[0].url}' alt="Event Banner">
-                `).addClass('eventClass')
-              $('.event-10').html(`
-                <h1> ${json._embedded.events[9].name}  </h1>
+                `)
+              $('#event-10').html(`
+                <h2> ${json._embedded.events[9].name}</h2>
                 <h4> Genre: ${json._embedded.events[9].classifications[0].segment.name}</h4>
                 <h4> Event type:  ${json._embedded.events[9].classifications[0].genre.name}</h4>
                 <h4> Date: ${json._embedded.events[9].dates.start.localDate}<h4>
                 <h4> Venue: ${json._embedded.events[9]._embedded.venues[0].name}<h4>
                 <img src= '${json._embedded.events[9].images[0].url}' alt="Event Banner">
-              `).addClass('eventClass')
+              `)
          },
           error: function(xhr, status, err) {
               $('.event-info').html(`<h2> No events found in your area </h2>`)
@@ -191,15 +218,21 @@ const getEvent = () => {
 });
 }
 
-$(() => {
 
-// Weather Carousel Variables
-let currentWeatherIndex = 0
-const highestWeatherIndex = $('.weather-info').children().length - 1;
 
-// Event Carousel Variables
-let currentEventIndex = 0
-const highestEventIndex = $('.event-info').children().length - 1;
+//////////////////////////////////////////////////
+
+/////////////
+// Old code for carousel
+///////////////////
+
+// // Weather Carousel Variables
+// let currentWeatherIndex = 0
+// const highestWeatherIndex = $('.weather-info').children().length - 1;
+//
+// // Event Carousel Variables
+// let currentEventIndex = 0
+// const highestEventIndex = $('.event-info').children().length - 1;
 
 //////////////
 // Event Listener
@@ -209,54 +242,52 @@ const highestEventIndex = $('.event-info').children().length - 1;
 //////////
 
 // Next button
-$('#next-weather').on('click', () => {
-  console.log('next weather tile');
-  $('.weather-info').children().eq(currentWeatherIndex).css('display', 'none');
-    if (currentWeatherIndex < highestWeatherIndex) {
-      currentWeatherIndex++;
-    } else {
-      currentWeatherIndex = 0;
-    }
-    $('.weather-info').children().eq(currentWeatherIndex).css('display', 'block');
-  })
-
-// Previous button
-$('#prev-weather').on('click', () => {
-  console.log('previous weather tile');
-  $('.weather-info').children().eq(currentWeatherIndex).css('display', 'none');
-    if (currentWeatherIndex > 0) {
-      currentWeatherIndex--;
-    } else {
-      currentWeatherIndex = highestWeatherIndex;
-    }
-    $('.weather-info').children().eq(currentWeatherIndex).css('display', 'block');
-  })
+// $('#next-weather').on('click', () => {
+//   console.log('next weather tile');
+//   $('.weather-info').children().eq(currentWeatherIndex).css('display', 'none');
+//     if (currentWeatherIndex < highestWeatherIndex) {
+//       currentWeatherIndex++;
+//     } else {
+//       currentWeatherIndex = 0;
+//     }
+//     $('.weather-info').children().eq(currentWeatherIndex).css('display', 'block');
+//   })
+//
+// // Previous button
+// $('#prev-weather').on('click', () => {
+//   console.log('previous weather tile');
+//   $('.weather-info').children().eq(currentWeatherIndex).css('display', 'none');
+//     if (currentWeatherIndex > 0) {
+//       currentWeatherIndex--;
+//     } else {
+//       currentWeatherIndex = highestWeatherIndex;
+//     }
+//     $('.weather-info').children().eq(currentWeatherIndex).css('display', 'block');
+//   })
 
 // Event Carousel
 ///////////
 
 // Next button
-$('#next-event').on('click', () => {
-  console.log('next event tile');
-  $('.event-info').children().eq(currentEventIndex).css('display', 'none')
-    if (currentEventIndex < highestEventIndex) {
-      currentEventIndex++;
-    } else {
-      currentEventIndex = 0;
-    }
-  $('.event-info').children().eq(currentEventIndex).css('display', 'block')
-})
-
-// Previous button
-$('#prev-event').on('click', () => {
-  console.log('previous weather tile');
-  $('.event-info').children().eq(currentEventIndex).css('display', 'none');
-    if (currentEventIndex > 0) {
-      currentEventIndex--;
-    } else {
-      currentEventIndex = highestEventIndex;
-    }
-    $('.event-info').children().eq(currentEventIndex).css('display', 'block');
-  })
-
-})
+// $('#next-event').on('click', () => {
+//   console.log('next event tile');
+//   $('.event-info').children().eq(currentEventIndex).css('display', 'none')
+//     if (currentEventIndex < highestEventIndex) {
+//       currentEventIndex++;
+//     } else {
+//       currentEventIndex = 0;
+//     }
+//   $('.event-info').children().eq(currentEventIndex).css('display', 'block')
+// })
+//
+// // Previous button
+// $('#prev-event').on('click', () => {
+//   console.log('previous weather tile');
+//   $('.event-info').children().eq(currentEventIndex).css('display', 'none');
+//     if (currentEventIndex > 0) {
+//       currentEventIndex--;
+//     } else {
+//       currentEventIndex = highestEventIndex;
+//     }
+//     $('.event-info').children().eq(currentEventIndex).css('display', 'block');
+//   })
